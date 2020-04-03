f434f66ae266c191f22291b4e84b138f1bac401b
truongcv <truong.cv@seesaa.vn>
2020-04-03 14:18:28 +0700

    init project

---
+++ MyPickerImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPickerImages
+//
+//  Created by TruongCV on 4/3/20.
+//  Copyright © 2020 TruongCV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
